557b8b619d3dffe033db085b652093e34347aa00
Ansh Srivastava <srivastavaansh1408@gmail.com>
2025-07-21 13:20:58 +0530

    first commit

--- real_time_traffic_analysis.py
+++ real_time_traffic_analysis.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 # Load the best fine-tuned YOLOv8 model
 best_model = YOLO('models/best.pt')
-
+  
 # Define the threshold for considering traffic as heavy
 heavy_traffic_threshold = 10
 
